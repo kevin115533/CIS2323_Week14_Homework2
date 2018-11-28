13e0fbf18c625214e7bd5ff04a24a6eb6890ab42
kevin115533 <kevin115533@yahoo.com>
2018-11-27 20:47:16 -0600

    it doesnt work

---
+++ app/src/main/java/com/example/ktran/sensorapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ktran.sensorapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    SensorManager sMgr = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+    TextView tv1=null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView2);
+        tv1.setVisibility(View.GONE);
+
+        List<Sensor> mList = sMgr.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 1; i < mList.size(); i++) {
+            tv1.setVisibility(View.VISIBLE);
+            tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
+    }
+}
